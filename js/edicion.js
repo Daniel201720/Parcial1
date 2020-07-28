883c39509359869052e91d805b1629b2e012bf05
Frans guillermo <fransguitar@gmail.com>
2020-07-28 05:41:37 -0500

    parte principal terminado, falta el enlace entra paginas

--- js/edicion.js
+++ js/edicion.js
@@ -1,3 +1,64 @@
-$(document).find('.tbl_user_data').html(tbl);
-$(document).find('.btn_save').hide();
-$(document).find('.btn_cancel').hide();
+$(document).ready(function(){
+  $(".selc_ok").hide();
+  $(".form-control").hide();
+
+  var calorias;
+  var grasas;
+  var proteina;
+  var carboidratos;
+
+  $("#row .1tx_edit").click(function () { 
+    $("#row .row_data").hide();
+    $("#row i").hide();
+    $("#row .form-control").show();
+    $("#selc_ok").show();
+    $("#slc1").show();
+
+   
+    
+  });
+  $("#row1 .1tx_edit").click(function () { 
+    $("#row1 .row_data").hide();
+    $("#row1 i").hide();
+    $("#row1 .form-control").show();
+    $("#selc_ok").show();
+    $("#slc2").show();
+   
+    
+  });
+  $("#row2 .1tx_edit").click(function () { 
+    $("#row2 .row_data").hide();
+    $("#row2 i").hide();
+    $("#row2 .form-control").show();
+    $("#selc_ok").show();
+    $("#slc3").show();
+
+   
+    
+  });
+  $("#row3 .1tx_edit").click(function () { 
+    $("#row3 .row_data").hide();
+    $("#row3 i").hide();
+    $("#row3 .form-control").show();
+    $("#selc_ok").show();
+    $("#slc4").show();
+
+   
+    
+  });
+  
+  $("#reset").click(function () { 
+    location. reload();
+    
+  });
+
+  $("#save").click(function () { 
+    $.post("demo_test.asp", function(data, status){
+      alert("Data: " + data + "\nStatus: " + status);
+    });
+    
+  });
+
+    
+   
+  });
